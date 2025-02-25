413170b67f31ca3302d217d74d465548a89ec7e6
Johannes Förstner <johannes@famfoerstner.de>
2025-02-25 16:07:36 +0100

    Add pre-build checks to build script

--- build.bat
+++ build.bat
@@ -13,6 +13,27 @@ set copyright=GPG 2002
 set author=Gas Powered Games
 set title=%map_cs%
 
+:: param
+set mode=%1
+echo %mode%
+
+:: pre-build checks
+setlocal EnableDelayedExpansion
+if not "%mode%"=="light" (
+  robocopy "%bits%\world\contentdb\gitignore" "%bits%\world\contentdb\templates\original" /S
+  pushd %gaspy%
+  set checks=standard
+  if "%mode%"=="release" (
+    set checks=all
+  )
+  venv\Scripts\python -m build.pre_build_checks %map% --check !checks! --bits "%bits%"
+  set pre_build_checks_errorlevel=!errorlevel!
+  rmdir /S /Q "%bits%\world\contentdb\templates\original"
+  if !pre_build_checks_errorlevel! neq 0 pause
+  popd
+)
+endlocal
+
 :: Compile map file
 rmdir /S /Q "%tmp%\Bits"
 robocopy "%bits%\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E
--- cleanup.bat
+++ cleanup.bat
@@ -1,8 +1,10 @@
 :: name of map, case-sensitive
 set map_cs=Yesterhaven
+
 :: path of DS installation
 set ds=%DungeonSiege%
 
 :: Cleanup resources so as not to confuse Siege Editor
+del "%ds%\Maps\%map_cs%.dsmap"
 del "%ds%\Resources\%map_cs%.dsres"
 del "%ds%\Resources\%map_cs%.*.dsres"
