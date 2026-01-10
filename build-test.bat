111c0b1cb13dcf4ad0dafbca3c96dcdd1a66f751
Johannes Förstner <johannes@famfoerstner.de>
2026-01-10 18:34:20 +0100

    Simplify build for test map (& update year fwiw)

--- build-test.bat
+++ build-test.bat
@@ -17,23 +17,6 @@ set title=%map_cs%
 set mode=%1
 echo %mode%
 
-:: pre-build checks
-setlocal EnableDelayedExpansion
-if not "%mode%"=="light" (
-  robocopy "%bits%\world\contentdb\gitignore" "%bits%\world\contentdb\templates\original" /S
-  pushd %gaspy%
-  set checks=standard
-  if "%mode%"=="release" (
-    set checks=all
-  )
-  venv\Scripts\python -m build.pre_build_checks %map% --check !checks! --bits "%bits%"
-  set pre_build_checks_errorlevel=!errorlevel!
-  rmdir /S /Q "%bits%\world\contentdb\templates\original"
-  if !pre_build_checks_errorlevel! neq 0 pause
-  popd
-)
-endlocal
-
 :: Compile main resource file
 rmdir /S /Q "%tmp%\Bits"
 robocopy "%bits%\art" "%tmp%\Bits\art" /E
@@ -45,32 +28,13 @@ robocopy "%bits%\world\global" "%tmp%\Bits\world\global" /E
 "%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\Resources\%map_cs%.dsres" -copyright "%copyright%" -title "%title%" -author "%author%"
 if %errorlevel% neq 0 pause
 
-set copyright=CC-BY-SA 2025
+set copyright=CC-BY-SA 2026
 set author=Johannes Förstner
 
 :: Compile map file
 rmdir /S /Q "%tmp%\Bits"
 robocopy "%bits%\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E
-pushd %gaspy%
-venv\Scripts\python -m build.fix_start_positions_required_levels %map% --bits "%tmp%\Bits"
-if %errorlevel% neq 0 pause
-setlocal EnableDelayedExpansion
-if "%mode%"=="release" (
-  robocopy "%bits%\world\contentdb\gitignore" "%bits%\world\contentdb\templates\original" /S
-  venv\Scripts\python -m build.add_world_levels %map% --bits "%tmp%\Bits" --template-bits "%bits%" --template-base original
-  set add_world_levels_errorlevel=!errorlevel!
-  rmdir /S /Q "%bits%\world\contentdb\templates\original"
-  if !add_world_levels_errorlevel! neq 0 pause
-)
-endlocal
-popd
 "%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\Maps\%map_cs%.dsmap" -copyright "%copyright%" -title "%title%" -author "%author%"
-if %errorlevel% neq 0 pause
-
-:: Compile German language resource file
-rmdir /S /Q "%tmp%\Bits"
-robocopy "%bits%\language" "%tmp%\Bits\language" *.de.gas /E
-"%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\Resources\%map_cs%.de.dsres" -copyright "%copyright%" -title "%title%" -author "%author%"
 if %errorlevel% neq 0 pause
 
 :: Cleanup
